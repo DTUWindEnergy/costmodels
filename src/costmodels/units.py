635c07fbccd309d439594f38496507aa84c8f13b
ernie <ernim@dtu.dk>
2025-03-28 13:15:19 +0100

    cm win test

--- src/costmodels/units.py
+++ src/costmodels/units.py
@@ -6,6 +6,6 @@ ureg = pint.UnitRegistry()
 ureg.define("EUR = [currency]")
 ureg.define("USD = [currency]")
 ureg.define("DKK = [currency]")
-ureg.formatter.default_format = "~#P"
+ureg.formatter.default_format = "~P"
 pint.set_application_registry(ureg)
 Quant: TypeAlias = pint.Quantity
