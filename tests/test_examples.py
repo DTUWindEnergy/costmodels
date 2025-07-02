559ac6389f3502b8cc91b653ebbdc4a61ba191a6
ernie <ernim@dtu.dk>
2025-07-02 14:23:24 +0200

    add excludes for example runs

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,8 +7,9 @@ import pytest
 
 EXAMPLES_DIR = Path(__file__).resolve().parents[1] / "examples"
 scripts = glob.glob(str(EXAMPLES_DIR / "*.py"))
+exclude = ["topfarm_integration.py"]
 
 
-@pytest.mark.parametrize("script", scripts)
+@pytest.mark.parametrize("script", [s for s in scripts if Path(s).name not in exclude])
 def test_example_runs(script):
     subprocess.run([sys.executable, str(EXAMPLES_DIR / script)], check=True)
